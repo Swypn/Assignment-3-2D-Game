d3c97b36bd5662c624aa6e66b88a405135372284
swypn <willy.pe00@gmail.com>
2023-12-05 14:41:22 +0100

    I added ranged for loop fpr InitializeBricks
    I added ranged for loop fpr InitializeBricks

--- pineapple/runner/src/Brick.cpp
+++ pineapple/runner/src/Brick.cpp
@@ -25,19 +25,18 @@ void BrickContainer::Update()
 void BrickContainer::InitializeBricks()
 {
 	m_brickObject.clear();
-
 	m_brickObject.resize(13);
 	float x = 0;
 	float y = 100;
-	for (int i = 0; i < m_brickObject.size(); i++)
-	{
 
-		m_brickObject[i].sprite.setTexture(m_texture);
-		m_brickObject[i].color = sf::Color::Red;
-		m_brickObject[i].positionX = x;
-		m_brickObject[i].positionY = y;
+	for(auto& bricks : m_brickObject)
+	{
+		bricks.sprite.setTexture(m_texture);
+		bricks.color = sf::Color::Red;
+		bricks.positionX = x;
+		bricks.positionY = y;
 		x += 100;
-		m_brickObject[i].sprite.setPosition(m_brickObject[i].positionX, m_brickObject[i].positionY);
-		m_brickObject[i].sprite.setColor(m_brickObject[i].color);
-	};
+		bricks.sprite.setPosition(bricks.positionX, bricks.positionY);
+		bricks.sprite.setColor(bricks.color);
+	}
 }
